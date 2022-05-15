3fe500a778bc71877f42da858efe2779118183e2
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-05-14 22:22:02 -0400

    nomad:1.3.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -25,7 +25,7 @@ dl_all () {
 
     if [ ! -e $lchecksums ];
     then
-        wget -q -O $lchecksums $rchecksums
+        curl -sSLf -o $lchecksums $rchecksums
     fi
 
     printf "  # %s\n" $rchecksums
@@ -39,4 +39,4 @@ dl_all () {
     dl $ver $lchecksums windows amd64
 }
 
-dl_all ${1:-1.2.6}
+dl_all ${1:-1.3.0}
